ba4a0db399c59ee7dbc05152136c1bae29a8fe0a
yxxr1 <yr.7ce.rn@gmail.com>
2025-01-21 22:34:40 +0300

    lab 3

--- LabLibrary/LabLibrary/Plane.cs
+++ LabLibrary/LabLibrary/Plane.cs
@@ -2,13 +2,33 @@
 {
     public class Plane
     {
-        public string Destination;
-        public string FlightId;
-        public string CompanyName;
-        public DateTime DepartureDateTime;
-        public int FlightPrice;
+        private string destination;
+        private DateTime departureDateTime;
+        private int flightPrice;
 
-        public Plane(string flightId, string companyName, string destination, DateTime dateTime, int price) {
+        public string Destination {
+            get => destination;
+            private set {
+                destination = char.ToUpper(value[0]) + value.Substring(1);
+            }
+        }
+        public string FlightId { get; private set; }
+        public string CompanyName { get; private set; }
+        public DateTime DepartureDateTime {
+            get => departureDateTime;
+            private set {
+                departureDateTime = value.DayOfWeek == DayOfWeek.Saturday ? value.AddDays(2) : value.DayOfWeek == DayOfWeek.Sunday ? value.AddDays(1) : value;
+            }
+        }
+        public int FlightPrice {
+            get => flightPrice;
+            private set {
+                flightPrice = Math.Max(value, 100);
+            }
+        }
+
+        public Plane(string flightId, string companyName, string destination, DateTime dateTime, int price)
+        {
             this.Destination = destination;
             this.FlightId = flightId;
             this.CompanyName = companyName;
